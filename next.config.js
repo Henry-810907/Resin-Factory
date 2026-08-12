79648dbe8b220762bcdbafa8ff5c8aec2863910d
root <root@localhost.localdomain>
2026-08-12 16:47:38 +0800

    Enable Next.js image caching (30 days TTL)

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30天 - 减少服务器图片转换开销
   },
   async headers() {
     return [
