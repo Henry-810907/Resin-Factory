b215f988f22196b6ee2a74b4d7bee70e3b58b78e
Henry <henry@resin-factory.com>
2026-08-13 14:33:41 +0800

    优化图片清晰度：添加高分辨率设备尺寸支持

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2560, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30天 - 减少服务器图片转换开销
   },
